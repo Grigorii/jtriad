dad49d198de78d6442bf13210299d5cd642081df
Grigoriy Kolevatov <tha.mad.monia@gmail.com>
2014-04-21 21:02:28 +0400

    Initial commit

---
+++ triad.core/src/Simulation.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Grigoriy on 21.04.2014.
+ */
+public class Simulation {
+    public void simulate()
+    {
+        System.out.println("Simulation starts");
+    }
+
+    public static void main(String args[])
+    {
+        new Simulation().simulate();
+    }
+}
